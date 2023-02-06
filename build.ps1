d855cd938a578a9f7dfb861cb156e4c0c13defe3
Matthew Pinkston <121766405+MatthewPinkston@users.noreply.github.com>
2023-02-06 14:02:17 -0500

    Create necessary empty directories

--- build.ps1
+++ build.ps1
@@ -1,3 +1,25 @@
+# Creates necessary empty directories for volume mapping
+If(!(test-path -PathType container $PSScriptRoot/config/ssl))
+{
+      New-Item -ItemType Directory -Path $PSScriptRoot/config/ssl
+}
+If(!(test-path -PathType container $PSScriptRoot/www))
+{
+      New-Item -ItemType Directory -Path $PSScriptRoot/www
+}
+If(!(test-path -PathType container $PSScriptRoot/data))
+{
+      New-Item -ItemType Directory -Path $PSScriptRoot/data
+      New-Item -ItemType Directory -Path $PSScriptRoot/data/mysql
+}
+If(!(test-path -PathType container $PSScriptRoot/logs))
+{
+      New-Item -ItemType Directory -Path $PSScriptRoot/logs
+      New-Item -ItemType Directory -Path $PSScriptRoot/logs/apache2
+      New-Item -ItemType Directory -Path $PSScriptRoot/logs/mysql
+      New-Item -ItemType Directory -Path $PSScriptRoot/logs/xdebug
+}
+
 docker compose --env-file "./www/.env" up -d
 docker exec --workdir "/var/www/html/www" IT-WebMgmt-LAMP-php81 composer install
 docker exec --workdir "/var/www/html/www" IT-WebMgmt-LAMP-php81 ./vendor/bin/phinx migrate
